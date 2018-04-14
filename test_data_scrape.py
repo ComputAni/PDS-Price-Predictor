22b89ee84ba3d031fb5aad78b9d92db5390dfdaa
Ani Ramakrishnan <anirudhr@andrew.cmu.edu>
2018-04-14 14:36:39 -0400

    Modified script to batch up requests for 1M. Cleans data and writes to two csv files (b/c large)

--- test_data_scrape.py
+++ test_data_scrape.py
@@ -19,8 +19,33 @@ client = Socrata("data.cityofnewyork.us", None)
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("uacg-pexx", limit=2000)
+max_size = 50000
+total_size = 1000000
+num_chunks = total_size // max_size #Should be 200
+total_data = []
+print("Requesting: %d chunks" % num_chunks);
+for i in range(num_chunks):
+    curr_offset = max_size*i
+    results = client.get("uacg-pexx", limit=50000, offset=curr_offset)
+    total_data += results
 
 # Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
-print(results_df)
+results_df = pd.DataFrame.from_records(total_data)
+print("Before scrubbing data size: ", len(results_df))
+results_df = results_df[results_df.pickup_latitude != '0']
+results_df = results_df[results_df.pickup_longitude != '0']
+results_df = results_df[results_df.dropoff_latitude != '0']
+results_df = results_df[results_df.dropoff_longitude != '0']
+print("After scrubbing data size: ", len(results_df))
+
+dlen = len(results_df)
+results_p1 = results_df.iloc[: dlen // 2]
+results_p2 = results_df.iloc[dlen // 2:]
+
+print(len(results_p1), len(results_p2))
+
+#Write to csv file
+results_p1.to_csv('cab_data_p1.csv')
+results_p2.to_csv('cab_data_p2.csv')
+
+print("Finished writing to file!")
